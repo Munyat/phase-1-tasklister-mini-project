366c9c6c3ec6e8484cc02802d46d3a6c6d25d70a
Brian Kipkirui Cheruiyot <briankipkiruimunyat@gmail.com>
2024-04-08 13:42:25 +0300

    task lister project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.getElementById("create-task-form");
+  let todo = document.getElementById("tasks");
+
+  //Form submit event
+  form.addEventListener("submit", addtodo);
+  //Delete event
+  todo.addEventListener("click", removeList);
+
+  //Add list
+  function addtodo(e) {
+    e.preventDefault();
+    //Get input value
+    let newtodo = document.getElementById("new-task-description").value;
+
+    //create new li element
+    let li = document.createElement("li");
+
+    //add text node with input value
+    li.appendChild(document.createTextNode(newtodo));
+
+    //create delete button
+    let deleteBtn = document.createElement("button");
+    //create text node
+    deleteBtn.appendChild(document.createTextNode("x"));
+    //append delete to li
+    li.appendChild(deleteBtn);
+
+    todo.appendChild(li);
+  }
+  function removeList(e) {
+    if (e.target.matches("button")) {
+      let li = e.target.parentElement;
+      todo.removeChild(li);
+    }
+  }
 });
